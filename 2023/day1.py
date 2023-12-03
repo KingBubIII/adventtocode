24a38a4c8d5e4f99092119b4a925a9ba42509598
Caleb Richardson <richardsoncaleb60@gmail.com>
2023-12-02 21:15:57 -0700

    day 1 part 2

--- 2023/day1.py
+++ 2023/day1.py
@@ -1,19 +1,53 @@
-with open('day1_input.txt',"r") as myfile:
+def get_ffd(myinput):
+    word_digit_options = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    span = (0,0)
+    while len(word_digit_options) > 0:
+        if myinput[span[0]:span[1]] == word_digit_options[0]:
+            return word_digit_options[0]
+        
+        curr_char = myinput[span[1]:span[1]+1]
+        if curr_char.isnumeric():
+            return curr_char
+        
+        span = (span[0], span[1]+1)
+        word_digit_options = list( filter(lambda digit_word: str(myinput[span[0]:span[1]]) in digit_word, word_digit_options) )
+
+    return get_ffd(myinput[span[0]+1:])
+
+def get_fld(myinput):
+    word_digit_options = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    span = (len(myinput)-1,len(myinput)-1)
+    while len(word_digit_options) > 0:
+        if myinput[span[0]:span[1]] == word_digit_options[0]:
+            return word_digit_options[0]
+        
+        curr_char = myinput[span[0]-1:span[0]]
+        if curr_char.isnumeric():
+            return curr_char
+        
+        span = (span[0]-1, span[1])
+        word_digit_options = list( filter(lambda digit_word: str(myinput[span[0]:span[1]]) in digit_word, word_digit_options) )
+
+    return get_fld(myinput[:span[1]])
+
+with open('2023\\day1_input.txt',"r") as myfile:
     all_lines = myfile.readlines()
 
     total = 0
-    digit_words = {"one":1, "two":2, "three":3, "four":4, "five":5, "six":6, "seven":7, "eight":8, "nine":9}
+    word_to_num = {"one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
 
     for line in all_lines:
-        digit1 = 0
-        digit2 = len(line)-1
-        while not line[digit1].isnumeric() or not line[digit2].isnumeric():
-            if not line[digit1].isnumeric():
-                digit1 += 1
+        digit1 = get_ffd(line)
+        digit2 = get_fld(line)
 
-            if not line[digit2].isnumeric():
-                digit2 -= 1
-        print(line[digit1] + line[digit2])
-        total += int(line[digit1] + line[digit2])
+        if len(digit1) > 1:
+            digit1 = word_to_num[digit1]
+        if len(digit2) > 1:
+            digit2 = word_to_num[digit2]
+
+        combined = digit1+digit2
+        total += int(combined)
+
+        print(combined)
 
     print(total)
