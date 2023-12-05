b8ce4cbfa86850684cee7aa6a72eaee53d82c926
Caleb Richardson <richardsoncaleb60@gmail.com>
2023-12-04 22:27:23 -0700

    day 4 part 1 *working*

--- 2023/day4/day4.py
+++ 2023/day4/day4.py
@@ -2,19 +2,19 @@ with open('2023/day4/day4_input.txt',"r") as myfile:
     cards = myfile.readlines()
     total = 0
     for card in cards:
+        card_val = 1
         unnessessary, card_info = card.strip().split(": ")
-        winning_numbers, actual_numbers = card_info.strip().split(" | ")
+        winning_numbers, actual_numbers = card_info.strip().replace("  "," ").split(" | ")
 
-        winning_num_set = set(winning_numbers.split(' '))
-        actual_numbers = set(actual_numbers.split(' '))
-        card_val = 0
-        for number in actual_numbers:
-            if number in winning_num_set:
-                if card_val == 0:
-                    card_val += 1
-                else:
-                    card_val *= 2
-                
+        winning_num_set = list(winning_numbers.split(' '))
+        actual_numbers = list(actual_numbers.split(' '))
+        common = list(set(winning_num_set) & set(actual_numbers))
+        
+        if len(common) > 0:
+            card_val *= 2 ** (len(common) - 1)
+        else:
+            card_val = 0
+        # print(common)
         print(card_val)
         total += card_val
     print(total)
